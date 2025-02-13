d970903b86874bb94fae9d3b16f4ae751006e314
Fidelio Ciandy <fiocheah08@gmail.com>
2025-02-13 15:58:49 -0600

    init project react

---
+++ blockchain_explorer/src/Home.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>Test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ blockchain_explorer/src/Navbar.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
