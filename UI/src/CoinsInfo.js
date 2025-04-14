83a80f6e84d05c24d1c3fab0339d50940ddd2632
Fidelio Ciandy <fiocheah08@gmail.com>
2025-04-14 15:48:01 -0500

    change structure

---
+++ UI/src/CoinsInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CoinsInfo() {
+  return (
+    <div>
+      <h1>Coins info Page</h1>
+    </div>
+  );
+}
+
+export default CoinsInfo;
---
+++ UI/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home";
+import Navbar from "./general/Navbar";
+import App from "./App";
+import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
